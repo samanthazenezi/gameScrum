fadb9b9ea381aada8ee87d37d2902d28a84ea5c7
Samantha Zenezi <samanthazenezi@gmail.com>
2024-01-22 11:58:57 -0300

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
+import { LoginComponent } from './pages/login/login.component';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    { path: '', component: LoginComponent},
+    { path:'home', component: HomeComponent }
 ];
--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './modal.component.css'
 })
 export class ModalComponent {
-
+  
 }
+
+
